622b2459f4b878297dc55963ae0deff4c60ad036
Amandaanderssn <tindraamanda@hotmail.se>
2025-05-09 15:27:45 +0200

    upd

--- src/Api/qrCodeApi/index.ts
+++ src/Api/qrCodeApi/index.ts
@@ -2,12 +2,12 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
 
 const qrCodeApi = createApi({
     reducerPath: 'qrCodeApi',
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3001/' }),
+    baseQuery: fetchBaseQuery({ baseUrl: 'http(s)://api.qrserver.com/v1/create-qr-code/' }),
     endpoints: (builder) => ({
         postQrCode: builder.mutation({
             // eslint-disable-next-line @typescript-eslint/no-explicit-any
             query: (newQrCode: any) => ({
-                url: 'qrCode',
+                url: '?data=[URL-encoded-text]&size=[pixels]x[pixels]',
                 method: 'POST',
                 body: newQrCode,
             }),
